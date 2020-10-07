e741c5d5f29de0b4b89a56a5cf5ab9156b724ff2
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-10-07 11:22:35 +0900

    Create Project

---
+++ src/main/java/springboot/jpaManager/JpaManagerApplication.java
@@ -0,0 +1,13 @@
+package springboot.jpaManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaManagerApplication.class, args);
+	}
+
+}
