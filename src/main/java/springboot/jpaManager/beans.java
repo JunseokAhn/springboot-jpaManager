040ce2f0b9b39c23e744cb3fe41edf5548741a50
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2021-06-25 15:31:13 +0900

    Update ModelMapper config

--- src/main/java/springboot/jpaManager/beans.java
+++ src/main/java/springboot/jpaManager/beans.java
@@ -1,6 +1,7 @@
 package springboot.jpaManager;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,14 @@ public class beans {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
     }
 
 }
